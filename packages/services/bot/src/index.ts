9f03e65e9d893985e1004b73846fa0be4bf0a179
prtkjakhar <prtkjakhar@gmail.com>
2023-11-24 16:12:39 +0530

    removed botservice dependency from adapter

--- packages/services/bot/src/index.ts
+++ packages/services/bot/src/index.ts
@@ -1,12 +1,9 @@
 export {
   getBotNodeFromStartingMessage,
   getBotNodeFromName,
-  getAdapterCredentials,
   getBotNodeFromId,
   getBotIdFromBotName,
   updateUser,
-  getAdapterByID,
-  getVaultCredentials,
   getFirstFormByBotID,
   getBotNameByBotID,
 } from './bot.service';
