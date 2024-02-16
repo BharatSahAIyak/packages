b7e2374ce416e194be7e7cfd2664c185a3c25169
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2024-02-16 17:15:02 +0530

    Added SimpleRetry and Classifier transformer.

--- packages/transformers/src/modules/common/transformer.types.ts
+++ packages/transformers/src/modules/common/transformer.types.ts
@@ -1,19 +1,22 @@
 export enum TransformerType {
     HTTP_POST = 'HTTP_POST',
     HTTP_GET = 'HTTP_GET',
-    SWITCH_CASE = 'SWITCH_CASE',
+    LABEL_CLASSIFIER = 'LABEL_CLASSIFIER',
     RANDOM_BINARY = 'RANDOM_BINARY',
+    SIMPLE_RETRY = 'SIMPLE_RETRY',
 }
 
 export enum TransformerClass {
     GenericTransformer,
     IfElseTransformer,
     SwitchCaseTransformer,
+    RetryTransformer,
 }
 
 export const TransformerMapping: Record<TransformerType, TransformerClass> = {
     [TransformerType.HTTP_POST]: TransformerClass.GenericTransformer,
     [TransformerType.HTTP_GET]: TransformerClass.GenericTransformer,
-    [TransformerType.SWITCH_CASE]: TransformerClass.SwitchCaseTransformer,
+    [TransformerType.LABEL_CLASSIFIER]: TransformerClass.SwitchCaseTransformer,
     [TransformerType.RANDOM_BINARY]: TransformerClass.IfElseTransformer,
+    [TransformerType.SIMPLE_RETRY]: TransformerClass.RetryTransformer,
 };
