ade84ce710ab2d52981b708eeee039226a0c841f
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2024-02-19 14:21:24 +0530

    Prevent overriding of existing metaData.

--- packages/transformers/src/modules/if_else/random_binary.transformer.ts
+++ packages/transformers/src/modules/if_else/random_binary.transformer.ts
@@ -7,11 +7,12 @@ export class RandomBinaryTransformer implements ITransformer {
 
     async transform(xmsg: XMessage): Promise<XMessage> {
         console.log("RANDOM_BINARY transformer used with: " + JSON.stringify(xmsg));
-        xmsg.transformer = {
-            metaData: {
-                state: Math.random() > 0.5 ? 'if' : 'else',
+        if (!xmsg.transformer) {
+            xmsg.transformer = {
+                metaData: {}
             }
         }
+        xmsg.transformer.metaData!.state = Math.random() > 0.5 ? 'if' : 'else';
         return xmsg;
     }
 }
