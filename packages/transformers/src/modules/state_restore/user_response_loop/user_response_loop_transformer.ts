175196fb04ecd48152d7ff8ba2b4b51a26f18d88
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2024-02-26 13:15:08 +0530

    Optionally accept XMessage.payload.text if prompt is not provided in UserFeedbackLoop transformer.

--- packages/transformers/src/modules/state_restore/user_response_loop/user_response_loop_transformer.ts
+++ packages/transformers/src/modules/state_restore/user_response_loop/user_response_loop_transformer.ts
@@ -4,14 +4,14 @@ import { ITransformer } from "../../common/transformer.interface";
 export class UserFeedbackLoopTransformer implements ITransformer {
 
     /// Accepted config properties:
-    ///     prompt: string: A prompt to send the user to reply to.
+    ///     prompt: string: A prompt to send the user to reply to. If not provided, `XMessage.payload.text` is required.
     ///     restoreState: string: The target state id to restore the state to.
     constructor(readonly config: Record<string, any>) { }
 
     async transform(xmsg: XMessage): Promise<XMessage> {
         console.log(`USER_FEEDBACK_LOOP called with: ${JSON.stringify(xmsg)}`);
-        if (!this.config.prompt) {
-            throw new Error('prompt is required!');
+        if (!this.config.prompt && !xmsg.payload.text) {
+            throw new Error('prompt or `XMessage.payload.text` is required!');
         }
         if (!this.config.restoreState) {
             throw new Error('restoreState is required!');
@@ -22,7 +22,7 @@ export class UserFeedbackLoopTransformer implements ITransformer {
             };
         }
         xmsg.transformer.metaData!.restoreState = this.config.restoreState;
-        xmsg.payload.text = this.config.prompt;
+        xmsg.payload.text = this.config.prompt || xmsg.payload.text;
         return xmsg;
     }
 }
