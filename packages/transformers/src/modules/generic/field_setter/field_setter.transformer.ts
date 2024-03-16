2ed168debc3b5f8931a1fa3af665ffe564d85faa
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2024-03-16 21:19:59 +0530

    Added JSON support to FIELD_SETTER.

--- packages/transformers/src/modules/generic/field_setter/field_setter.transformer.ts
+++ packages/transformers/src/modules/generic/field_setter/field_setter.transformer.ts
@@ -9,9 +9,9 @@ export class FieldSetterTransformer implements ITransformer {
     /// setters: Record<string, string> : This defines the fields that need to be set,
     ///          where the `key` is the parameter which needs to be set in `XMessage`
     ///          and `value` is the parameter in `XMessage` which should be used as value.
-    ///          If `value` is a normal string, the value will be used directly, if the value
-    ///          is enclosed in {} like {xmsg:payload.text} or {history:payload.text}, it will
-    ///          be extracted from `XMessage` or from the most recent history message.
+    ///          If `value` is a normal string or JSON, the value will be used directly, if
+    ///          the value is enclosed in {} like {xmsg:payload.text} or {history:payload.text},
+    ///          it will be extracted from `XMessage` or from the most recent history message.
     constructor(
         readonly config: Record<string, any>,
     ) { }
@@ -28,18 +28,18 @@ export class FieldSetterTransformer implements ITransformer {
         return xmsgCopy;
     }
 
-    private getResolvedValue(value: string, xmsg: XMessage): string {
+    private getResolvedValue(value: string, xmsg: XMessage): string | JSON {
         const xmsgPlaceholder = /\{xmsg:(.*)\}/;
         const historyPlaceholder = /\{history:(.*)\}/;
 
-        if (value.match(xmsgPlaceholder)) {
-            return get(xmsg, value.match(xmsgPlaceholder)![1]);
+        if (typeof value === 'string') {
+            if (value.match(xmsgPlaceholder)) {
+                return get(xmsg, value.match(xmsgPlaceholder)![1]);
+            }
+            else if (value.match(historyPlaceholder)) {
+                return get(xmsg.transformer?.metaData?.userHistory.pop() ?? {}, value.match(historyPlaceholder)![1]);
+            }
         }
-        else if (value.match(historyPlaceholder)) {
-            return get(xmsg.transformer?.metaData?.userHistory.pop() ?? {}, value.match(historyPlaceholder)![1]);
-        }
-        else {
-            return value;
-        }
+        return value;
     }
 }
