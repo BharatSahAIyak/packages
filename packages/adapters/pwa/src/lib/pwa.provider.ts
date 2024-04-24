2ede7de1ba04867fb2d1da69c0d6b5a016b2230b
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2024-04-25 01:19:18 +0530

    Accept XMessage as request format in pwa adapter.

--- packages/adapters/pwa/src/lib/pwa.provider.ts
+++ packages/adapters/pwa/src/lib/pwa.provider.ts
@@ -8,48 +8,19 @@ export class PwaBotProvider implements XMessageProvider {
 
     constructor(private config: PwaBotConfig) {}
 
-    async convertMessageToXMsg(msg: PwaBotMessage): Promise<XMessage> {
-        let messageType = MessageType.TEXT;
-        if (msg.messageType == MessageType.FEEDBACK_POSITIVE) {
-            messageType = MessageType.FEEDBACK_POSITIVE;
-        } 
-        else if (msg.messageType == MessageType.FEEDBACK_NEGATIVE) {
-            messageType = MessageType.FEEDBACK_NEGATIVE;
-        }
-        else if (msg.messageType == MessageType.FEEDBACK_NEUTRAL) {
-            messageType = MessageType.FEEDBACK_NEUTRAL;
-        }
-        let text = msg.body || '';
-        if (text.startsWith('\\register')) {
-            text = text.replace('\\register ', '').trim();
-            messageType = MessageType.REGISTRATION;
-        }
-        const xmessage: XMessage = {
-            to: {
-                userID: "admin",
-                bot: true,
-            },
-            from: {
-                userID: `${msg.userId}`,
-                bot: false,
-            },
-            channelURI: "Bot",
-            providerURI: "Pwa",
-            messageState: MessageState.REPLIED,
-            messageId: {
-                channelMessageId: msg.conversationId,
-                Id: `${msg.messageId}`,
-                replyId: msg.replyId,
-            },
-            messageType: messageType,
-            timestamp: new Date().getTime(),
-            payload: {
-                text: text,
-                metaData: msg.metaData
-            },
+    async convertMessageToXMsg(msg: XMessage): Promise<XMessage> {
+        msg.channelURI = 'Bot';
+        msg.providerURI = 'Pwa';
+        msg.to = {
+            userID: 'admin',
+            bot: true,
         };
-
-        return xmessage;
+        msg.from.bot = false;
+        msg.messageType = msg.messageType ?? MessageType.TEXT;
+        msg.messageState = MessageState.REPLIED;
+        msg.timestamp = Date.now();
+        msg.payload = msg.payload ?? {};
+        return msg;
     }
 
     async sendMessage(xmsg: XMessage) {
