484e6f58afe27b9203c740f13c3453324b889fa4
Varun Agarwal <104075461+1010varun@users.noreply.github.com>
2024-08-12 10:47:07 +0530

    feat: added support for long text in Discord Adapter (#145)

--- packages/adapters/discord/src/lib/discord.provider.ts
+++ packages/adapters/discord/src/lib/discord.provider.ts
@@ -3,28 +3,66 @@ import {
   IChatOptions,
   IChatProvider,
   ISendMessageSuccessResponse,
-} from '@novu/stateless';
-import axios from 'axios';
-import { DiscordBotProviderConfig } from './discord.bot.config';
+} from "@novu/stateless";
+import axios, { AxiosResponse } from "axios";
+import { DiscordBotProviderConfig } from "./discord.bot.config";
+import FormData from "form-data";
+import * as fs from "fs";
+import * as path from "path";
+import { v4 as uuidv4 } from "uuid";
 
 export class DiscordProvider implements IChatProvider {
   channelType = ChannelTypeEnum.CHAT as ChannelTypeEnum.CHAT;
-  public id = 'discord';
-  private axiosInstance = axios.create();
+  public id = "discord";
 
   constructor(private config: DiscordBotProviderConfig) {}
 
   async sendMessage(data: IChatOptions): Promise<ISendMessageSuccessResponse> {
     // Setting the wait parameter with the URL API to respect user parameters
     const url = new URL(this.config.webhookUrl);
-    url.searchParams.set('wait', 'true');
-    const response = await this.axiosInstance.post(url.toString(), {
-      content: data.content,
-    });
-
-    return {
-      id: response.data.id,
-      date: response.data.timestamp,
-    };
+    url.searchParams.set("wait", "true");
+    if (data.content.length <= 2000) {
+      const response = await axios.post(url.toString(), {
+        content: data.content,
+      })
+      .then((resp) => resp)
+      .catch((err) => {
+        console.error(err);
+        return undefined;
+      });
+      if (!response) {
+        throw new Error(`Could not send discord message.`);
+      }
+      return {
+        id: response.data.id, 
+        date: response.data.timestamp
+      };
+    }
+    // Else create a temp .txt file and send that file to the server
+    const tempFilePath = path.join(__dirname, `${uuidv4()}.txt`);
+    fs.writeFileSync(tempFilePath, data.content, "utf-8");
+    url.searchParams.set("wait", "true");
+    const form = new FormData();
+    form.append("file", fs.createReadStream(tempFilePath));
+    let response: AxiosResponse<any> | null = null;
+    try {
+      response = await axios.post(url.toString(), form, {
+        headers: {
+          ...form.getHeaders(),
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      fs.unlinkSync(tempFilePath);
+    }
+    if (response) {
+      return {
+        id: response.data.id,
+        date: response.data.timestamp,
+      };
+    } else {
+      throw new Error("Failed to get a response from the server");
+    }
   }
 }
