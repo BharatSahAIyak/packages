a8e1b2a6b7a40e58aa5f871801cece45d6cc9e7d
Yash Mittal <yash@techsavvyash.dev>
2024-09-03 11:22:36 +0530

    feat: add a new switch case transformer (#166)
    * feat: add a new switch case transformer
    
    * fix: review comments
    
    * fix: transformer name
    
    Co-authored-by: Chinmoy <chinmoy12c@gmail.com>
    
    * fix: transformer name
    
    Co-authored-by: Chinmoy <chinmoy12c@gmail.com>
    
    * fix: transformer name
    
    Co-authored-by: Chinmoy <chinmoy12c@gmail.com>
    
    ---------
    
    Co-authored-by: Chinmoy <chinmoy12c@gmail.com>

---
+++ packages/transformers/src/modules/switch_case/field_state/field_state.transformer.ts
@@ -0,0 +1,47 @@
+import { XMessage } from "@samagra-x/xmessage";
+import { ITransformer } from "../../common/transformer.interface";
+import { Events } from "@samagra-x/uci-side-effects";
+import get from 'lodash/get';
+import { TelemetryLogger } from "../../common/telemetry";
+
+export class FieldToStateTransformer{
+
+    config: Record<string, any>;
+    private readonly telemetryLogger: TelemetryLogger;
+
+    /// Accepted config properties:
+    ///     target: string:  flat path to the target field in the transformer, defaults to  `payload.text`
+    ///
+    constructor(config: Record<string, any>){ 
+        this.config = config;
+        this.telemetryLogger = new TelemetryLogger(this.config);
+    }
+    
+
+    async transform(xmsg: XMessage): Promise<XMessage> {
+        const startTime = Date.now();
+        this.telemetryLogger.sendLogTelemetry(xmsg, `FIELD STATE TRANSFORMER : ${this.config.transformerId} started`, startTime);
+        
+        if(!this.config.target){
+            this.config.target = 'payload.text';
+        }
+
+        if (!xmsg.transformer) {
+            xmsg.transformer = {
+                metaData: {}
+            };
+        }
+                    
+        let outputState = get(xmsg, this.config.target);
+        if (outputState != undefined) {
+            xmsg.transformer!.metaData!.state = outputState;
+        }
+        else {
+            xmsg.transformer!.metaData!.state = 'STATE_NOT_AVAILABLE';
+        }
+        
+        this.telemetryLogger.sendLogTelemetry(xmsg, `FIELD TO STATE Transformer generated state: ${xmsg.transformer!.metaData!.state}`, startTime);
+        console.log(`LABEL_CLASSIFIER generated state: ${xmsg.transformer!.metaData!.state}`);
+        return xmsg;
+    }
+}
