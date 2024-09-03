548d94785bbcbd0ee3448feca95fc0c01797fe7e
Yash Mittal <yash@techsavvyash.dev>
2024-09-03 18:12:13 +0530

    fix: export the field state transformer

--- packages/transformers/src/modules/switch_case/index.ts
+++ packages/transformers/src/modules/switch_case/index.ts
@@ -1,2 +1,3 @@
 export * from './label_classifier/label_classifier.transformer';
 export * from './message_type_classifier/message_type_classifier.transformer';
+export * from './field_state/field_state.transformer';
