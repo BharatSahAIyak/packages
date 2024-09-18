8c5dcb20d5de9524c275abbef5b790dbac939de8
AmruthVamshi <amruth.vamshi@esmagico.in>
2024-09-18 08:23:06 +0530

    Implemented custom code runner side effect.

---
+++ packages/sideEffects/src/codeRunner/codeRunner.sideEffect.ts
@@ -0,0 +1,72 @@
+import { ISideEffect } from "../common/sideEffect.interface";
+import { Events, SideEffectData } from "../common/sideEffect.types";
+const ivm = require('isolated-vm');
+import { XMessage } from "@samagra-x/xmessage";
+
+const AcceptedEvents: string[] = [
+    Events.CUSTOM_CODE_RUNNER,
+];
+
+export class CodeRunnerSideEffect implements ISideEffect {
+    constructor(private readonly config: Record<string, any>) {}
+
+    static getName(): string {
+        return "CodeRunner";
+    }
+
+    static doesConsumeEvent(eventName: string): Boolean {
+        return AcceptedEvents.includes(eventName);
+    }
+
+    async execute(sideEffectData: SideEffectData): Promise<boolean> {
+        try {
+            let xmsg = sideEffectData.eventData;
+            if (!this.config?.code) {
+                throw new Error('config.code is required');
+            }
+            if (!xmsg.transformer) {
+                xmsg.transformer = {
+                    metaData: {}
+                };
+            }
+            const isolate = new ivm.Isolate({ memoryLimit: 64 });
+            const context = isolate.createContextSync();
+            const jail = context.global;
+            jail.setSync('global', jail.derefInto());
+            const xmsgCopy: Partial<XMessage> = { };
+            xmsgCopy.payload = xmsg.payload;
+            xmsgCopy.transformer = xmsg.transformer;
+            const codeResult = context.evalClosureSync(
+                this.config.code,
+                [ JSON.stringify(xmsgCopy) ],
+                {
+                    timeout: 30_000,
+                    arguments: { 
+                        fetch: new ivm.Reference(async (url: string) => {
+                          // Perform fetch in the main Node.js environment
+                          const response = await fetch(url);
+                          const contentType = response.headers.get('content-type');
+                          let data;
+                          
+                          // Check the content type and parse accordingly
+                          if (contentType && contentType.includes('application/json')) {
+                            data = await response.json();
+                          } else {
+                            data = await response.text();
+                          }
+                          
+                          return data; // Return the parsed data to the VM
+                        }),
+                      },
+                      result: { promise: true } // Ensure we return promises
+                }
+            );
+            console.log('CodeRunner sideEffect return:',codeResult);
+            return true;
+        } catch (error) {
+            console.error("Error occurred during telemetry:", error);
+            return false;
+        }
+    }
+
+}
