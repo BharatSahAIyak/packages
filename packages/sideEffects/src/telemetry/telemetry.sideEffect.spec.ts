42e83f3f7f1c13d7a73cfddb8ff1b84035e3ac91
Chinmoy <chinmoy12c@gmail.com>
2024-09-19 13:23:53 +0530

    Fix side effect tests. (#193)

--- packages/sideEffects/src/telemetry/telemetry.sideEffect.spec.ts
+++ packages/sideEffects/src/telemetry/telemetry.sideEffect.spec.ts
@@ -38,18 +38,17 @@ const mockEvent = {
 
 const expectedRequest = [
     {
-      "generator": "1",
+      "generator": "Transformer",
       "version": "0.0.1",
-      "timestamp": 1712576582397,
-      "actorId": "1",
-      "actorType": "Transformer",
+      "actorId": "00000000-0000-0000-0000-000000000000",
+      "actorType": "System",
       "env": "UNKNOWN",
       "eventId": "E041",
       "event": "Transformer Execution",
       "subEvent": "DEFAULT_TRANSFORMER_START_EVENT",
       "eventData": {
         "botId": "00000000-0000-0000-0000-000000000000",
-        "userId": "00000000-0000-0000-0000-000000000000",
+        "transformerId": "1",
         "orgId": "00000000-0000-0000-0000-000000000000",
         "messageId": "00000000-0000-0000-0000-000000000000"
       }
@@ -61,7 +60,7 @@ describe(('Telemetry Side Effect Test'), () => {
     afterEach(() => fetchMock.restore());
 
     it('Telemetry works as expected', async () => {
-        let currentRequest;
+        let currentRequest: any[];
         fetchMock.postOnce('http://mytesturl/metrics/v1/save', (_, req) => {
             currentRequest = JSON.parse(`${req.body}`);
             return true;
@@ -76,7 +75,9 @@ describe(('Telemetry Side Effect Test'), () => {
             transformerId: mockEvent.transformerId,
         });
         expect(response).toBeTruthy();
-        expect(currentRequest).toStrictEqual(expectedRequest);
+        expect(currentRequest![0].timestamp);
+        delete currentRequest![0].timestamp;
+        expect(currentRequest!).toStrictEqual(expectedRequest);
     });
 
     it('Telemetry side-effect responds false in case of error', async () => {
